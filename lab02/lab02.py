a99c9790af292245722db808fd383caeb63f0536
Mohammed <mia5663@psu.edu>
2025-01-21 11:30:10 -0500

    Found the best 5 card hand out of 7 cards for poker.

--- lab02/lab02.py
+++ lab02/lab02.py
@@ -20,9 +20,9 @@ import itertools
 
 def best_hand(hand):
     "From a 7-card hand, return the best 5 card hand."
-    # Your code here
-    pass
-    
+    combinations = itertools.combinations(hand, 5)
+    return max(combinations, key = hand_rank)
+
 # ------------------
 # Provided Functions
 # 
