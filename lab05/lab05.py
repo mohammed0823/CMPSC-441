a8e81d7c07f891769badc82668dd2e7f4c84355a
Mohammed <mia5663@psu.edu>
2025-02-23 13:40:57 -0500

    Filled out process_response function and tool template

--- lab05/lab05.py
+++ lab05/lab05.py
@@ -23,9 +23,15 @@ def roll_for(skill, dc, player):
         return f'{player} rolled {roll} for {skill} and failed!'
 
 def process_response(self, response):
-    # Fill out this function to process the response from the LLM
-    # and make the function call 
+    if response.message.tool_calls:
+        self.messages.append({'role': 'tool',
+                              'name': response.message.tool_calls[0].function.name,
+                              'arguments': response.message.tool_calls[0].function.arguments,
+                              'content': process_function_call(response.message.tool_calls[0].function)
+                         })
+        response = self.completion()
     return response
+
 
 run_console_chat(template_file='lab05/lab05_dice_template.json',
                  process_response=process_response)
